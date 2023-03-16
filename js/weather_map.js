fe8a50d36a1ee57f49a4d6eb44c917f013f2e5d6
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-16 14:55:03 -0500

    fixed bug that affected firefox, also refactored current weather

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,15 +1,28 @@
 (function() {
   "use strict"
   let locationCoord = [-98.48527, 29.423017];
+  const zoomLevel = 10;
+  const dayCount = 4;
   setPage(locationCoord);
 
   mapboxgl.accessToken = mapboxKey;
   const map = new mapboxgl.Map({
     container: "map", // container ID
     style: "mapbox://styles/gagejackson/clf2s2gln001601qik4wqike1", // style URL
-    zoom: 12, // starting zoom
+    zoom: zoomLevel, // starting zoom
     center: locationCoord, // [lng, lat]
   });
+
+  const currentWeatherDiv = document.querySelector("#currentWeather");
+  const forecastedWeatherTilesDiv = document.querySelector("#forecastTiles");
+  const newLocationButton = document.querySelector("#btn-submit-address");
+  const weatherTabsDiv = document.getElementsByClassName("nav-link");
+  const weatherTabContentDiv = document.getElementsByClassName("tab-pane");
+
+  let marker = new mapboxgl.Marker({draggable: true, color: "#C3E6E7FF", className:"marker"})
+      .setLngLat(locationCoord)
+      .addTo(map);
+
 
   function setPage(locationCoord) {
     $.get("http://api.openweathermap.org/data/2.5/onecall", {
@@ -25,19 +38,12 @@
     });
   }
 
-  let marker = new mapboxgl.Marker({draggable: true, color: "#C3E6E7FF"})
-      .setLngLat(locationCoord)
-      .addTo(map);
-
-//marker.addEventListener("dragend", newLocation);
-
-  function onDragEnd() {
+  marker.on("dragend", () => {
     let dragCoords = marker.getLngLat();
     locationCoord = [dragCoords.lng, dragCoords.lat];
     setPage(locationCoord);
-  }
+  });
 
-  marker.on("dragend", onDragEnd);
   map.on("click", (e) => {
     let dragCoords = e.lngLat;
     locationCoord = [dragCoords.lng, dragCoords.lat];
@@ -45,18 +51,37 @@
     setPage(locationCoord);
   });
 
-  const currentWeatherDiv = document.querySelector("#currentWeather");
-  const forecastedWeatherTilesDiv = document.querySelector("#forecastTiles");
-  const newLocationButton = document.querySelector("#btn-submit-address");
-  const weatherTabsDiv = document.getElementsByClassName("nav-link");
-  const weatherTabContentDiv = document.getElementsByClassName("tab-pane");
+
+  function getDate(date) {
+    return new Date(date * 1000).toLocaleString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    });
+  }
+
+  function setLocationName(data) {
+    let locationName = "";
+    $.get("http://api.openweathermap.org/geo/1.0/reverse", {
+      APPID: weatherKey,
+      lat: data.lat,
+      lon: data.lon,
+    }).done(function (locationData) {
+      locationName = locationData[0].name;
+      setCurrentWeatherDiv(data, locationName);
+      setForecastedWeather(data);
+    });
+  }
+
+  function setForecastedWeather(data) {
+    forecastedWeatherTilesDiv.innerHTML = getForecastedWeatherTiles(data);
+  }
+
 
   newLocationButton.addEventListener("click", () => {
     let location = document.querySelector("#userLocation").value;
     geocode(location, mapboxKey).then(function (result) {
-      console.log(result);
-      //6354 creekview lane, fishers, in 46038
-      map.flyTo({center: result, zoom: 16});
+      map.flyTo({center: result, zoom: zoomLevel});
       marker.setLngLat(result);
       locationCoord = result;
       setPage(locationCoord);
@@ -68,6 +93,179 @@
       weatherTabsDiv[i].innerText = getDate(data.daily[i].dt);
     }
   }
+
+
+  /*
+  ///////////////////////////////////////////////////////////////////////
+  Determines which gif to pull from assets from api
+  ///////////////////////////////////////////////////////////////////////
+   */
+  function getWeatherGif(weatherState) {
+    let weatherGifSource = "";
+    if (weatherState < 200) {
+      weatherGifSource = "/assets/weather-gifs/rainbow.gif";
+    }
+    if (weatherState < 300 && weatherState >= 200) {
+      weatherGifSource = "/assets/weather-gifs/storm.gif";
+    }
+    if (weatherState < 400 && weatherState >= 300) {
+      weatherGifSource = "/assets/weather-gifs/drizzle.gif";
+    }
+    if (weatherState < 500 && weatherState >= 400) {
+      weatherGifSource = "/assets/weather-gifs/rainbow.gif";
+    }
+    if (weatherState < 600 && weatherState >= 500) {
+      if (weatherState === 511) {
+        weatherGifSource = "/assets/weather-gifs/icicles.gif";
+      }
+      weatherGifSource = "/assets/weather-gifs/rain.gif";
+    }
+    if (weatherState < 700 && weatherState >= 600) {
+      weatherGifSource = "/assets/weather-gifs/snow-2.gif";
+    }
+    if (weatherState < 800 && weatherState >= 700) {
+      if (weatherState === 781) {
+        weatherGifSource = "/assets/weather-gifs/whirlwind.gif";
+      }
+      weatherGifSource = "/assets/weather-gifs/foggy.gif";
+    }
+    if (weatherState === 800) {
+      weatherGifSource = "/assets/weather-gifs/sun.gif";
+    }
+    if (weatherState === 801) {
+      weatherGifSource = "/assets/weather-gifs/cloudy.gif";
+    }
+    if (weatherState >= 802) {
+      weatherGifSource = "/assets/weather-gifs/clouds.gif";
+    }
+    return weatherGifSource;
+  }
+
+
+  /*
+  ///////////////////////////////////////////////////////////////////////
+  Create Current Weather HTML
+  ///////////////////////////////////////////////////////////////////////
+   */
+  function setCurrentWeatherDiv(data, myLocation) {
+    let weatherGif = getWeatherGif(data.current.weather[0].id);
+    let html = "";
+    html += `<div class ='card container'>`;
+
+    html += `<div class="card-header row border-1 colorMe text-light">`;
+    html += `<h3 class="col-6 text-start">${myLocation}</h3>`;
+    html += `<h3 class="col-6 text-end">Today</h3>`;
+    html += `</div>`;//end header
+
+    html += `<div class="card-body row p-0">`;//start card body
+    html += `<div class="col-6 p-0 d-flex">`;//start col 1 of 2
+    html += `<img src=${weatherGif} alt="weather gif" class="img-fluid align-self-center">`;
+    html += `</div>`;//end col 1 of 2 for image
+
+    html += `<div class="col-6">`;//start col 2 of 2
+
+    html += `<div class="row mb-3 d-flex align-items-center">`;
+    html += `<div class="col-4 py-2">`;
+    html += `<img src="/assets/weather-png/temperature.png" alt="current temp" class="img-fluid">`;
+    html += `</div>`;
+    html += `<div class="col-8">`;
+    html += `<span class="">${Math.round(data.current.temp)}&#8457</span>`;
+    html += `</div>`;
+    html += `</div>`;//end row 1 of 3
+
+    html += `<div class="row bg-light mb-3 d-flex align-items-center">`;
+    html += `<div class="col-4 py-2">`;
+    html += `<img src="/assets/weather-png/umbrella.png" alt="current temp" class="img-fluid">`;
+    html += `</div>`;
+    html += `<div class="col-8">`;
+    html += `<span class="">${data.hourly[0].pop * 100}%</span>`;
+    html += `</div>`;
+    html += `</div>`;//end row 2 of 3
+
+    html += `<div class="row mb-3 d-flex align-items-center">`;
+    html += `<div class="col-4 py-2">`;
+    html += `<img src="/assets/weather-png/wind.png" alt="current temp" class="img-fluid">`;
+    html += `</div>`;
+    html += `<div class="col-8">`;
+    html += `<span class="">${data.current.wind_speed} mph</span>`;
+    html += `</div>`;
+    html += `</div>`;//end row 3 of 3
+
+    html += `</div>`;//end col 2 of 2
+    html += `</div>`;//end card body
+
+    html += `<div class="card-footer row">`;
+    html += `<h3 class="text-center">${data.current.weather[0].description}</h3>`;
+    html += `</div>`;//end Footer
+
+    html += `</div>`;//end Card
+    currentWeatherDiv.innerHTML = (html);
+  }
+
+
+  /*
+  ///////////////////////////////////////////////////////////////////////
+  Create Weather Tiles HTML
+  ///////////////////////////////////////////////////////////////////////
+   */
+  function getForecastedWeatherTiles(data) {
+    let forecastedDayCount = dayCount + 1;
+    let html = "";
+    for (let i = 1; i < forecastedDayCount; i++) {
+      let weatherGif = getWeatherGif(data.daily[i].weather[0].id);
+
+      html += '<div class="card col-5 col-md p-0 mx-auto mx-md-2 my-2">';
+      html += '<div class="card-header colorMe text-white p-3 p-md-2 pt-3">';
+      html += "<h3 class='text-center'>" + getDate(data.daily[i].dt) + "</h3>";
+      html += "</div>";
+
+
+      html += '<div class="card-body">';
+      html +=
+          "<img src=" +
+          weatherGif +
+          ' alt="weather gif" class="img-fluid align-self-center">';
+      html += "</div>";
+
+
+      html +=
+          '<div class="card-footer bg-light d-flex justify-content-between p-0 pt-3">';
+      html += '<div class="col-4 p-0 text-center">';
+      html +=
+          '<img src="/assets/weather-png/cold.png" alt="low temp" class="img-fluid w-50 mb-2">';
+      html +=
+          "<p>" +
+          Math.round(data.daily[i].temp.min) +
+          "<span class='unit'>&#8457</span></p>";
+      html += "</div>";
+
+
+      html += '<div class="col-4 p-0 text-center">';
+      html +=
+          '<img src="/assets/weather-png/hot.png" alt="high temp" class="img-fluid w-50 mb-2">';
+      html +=
+          "<p>" +
+          Math.round(data.daily[i].temp.max) +
+          "<span class='unit'>&#8457</span></p>";
+      html += "</div>";
+
+
+      html += '<div class="col-4 p-0 text-center">';
+      html +=
+          '<img src="/assets/weather-png/umbrella.png" alt="chance of rain" class="img-fluid w-50 mb-2">';
+      html +=
+          "<p>" +
+          Math.round(data.daily[i].pop * 100) +
+          "<span class='unit'>%</span></p>";
+      html += "</div>";
+
+
+      html += "</div>";
+      html += "</div>";
+    }
+    return html;
+  }
+
 
   /*
   ///////////////////////////////////////////////////////////////////////
@@ -131,7 +329,7 @@
       html +=
           '<img src="/assets/weather-png/sunrise.png" alt="morning temp" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].temp.morn) +
           "<span class='unit'>&#8457</span></p>";
       html += "</div>";
@@ -140,7 +338,7 @@
       html +=
           '<img src="/assets/weather-png/sun.png" alt="day temp" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].temp.day) +
           "<span class='unit'>&#8457</span></p>";
       html += "</div>"
@@ -149,7 +347,7 @@
       html +=
           '<img src="/assets/weather-png/night.png" alt="night temp" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].temp.night) +
           "<span class='unit'>&#8457</span></p>";
       html += "</div>";
@@ -161,7 +359,7 @@
       html +=
           '<img src="/assets/weather-png/clouds.png" alt="cloud coverage" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].clouds) +
           "<span class='unit'>%</span></p>";
       html += "</div>";
@@ -170,7 +368,7 @@
       html +=
           '<img src="/assets/weather-png/sun-icon.png" alt="uvi" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].uvi) +
           "<span class='unit'>uvi</span></p>";
       html += "</div>"
@@ -179,7 +377,7 @@
       html +=
           '<img src="/assets/weather-png/eco.png" alt="humidity" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].humidity) +
           "<span class='unit'>%</span></p>";
       html += "</div>";
@@ -191,7 +389,7 @@
       html +=
           '<img src="/assets/weather-png/weathercock.png" alt="wind direction" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].wind_deg) +
           "<span class='unit'>deg</span></p>";
       html += "</div>";
@@ -200,7 +398,7 @@
       html +=
           '<img src="/assets/weather-png/wind.png" alt="wind speed" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].wind_speed) +
           "<span class='unit'>mph</span></p>";
       html += "</div>"
@@ -209,7 +407,7 @@
       html +=
           '<img src="/assets/weather-png/gust.png" alt="gust speed" class="col-5 w-50 h-50 ratio ratio-1x1">';
       html +=
-          "<p class='col-5 p-0 text-nowrap'>" +
+          "<p class='col-5 p-0 text-nowrap my-auto'>" +
           Math.round(data.daily[i].wind_gust) +
           "<span class='unit'>mph</span></p>";
       html += "</div>";
@@ -222,192 +420,5 @@
       weatherTabContentDiv[i].innerHTML = html;
       html = "";
     }
-  }
-
-  /*
-  ///////////////////////////////////////////////////////////////////////
-  Create Current Weather HTML
-  ///////////////////////////////////////////////////////////////////////
-   */
-  function setCurrentWeatherDiv(data, myLocation) {
-    let weatherGif = getWeatherGif(data.current.weather[0].id);
-    currentWeatherDiv.setHTML(
-        `
-      <div class ='card container'>
-        <div class="card-header row border-1 colorMe text-light">
-            <h3 class="col-6 text-start">${myLocation}</h3>
-            <h3 class="col-6 text-end">Today</h3>
-        </div>
-        <div class="card-body row p-0">
-            <div class="col-6 p-0 d-flex">
-                <img src=${weatherGif} alt="weather gif" class="img-fluid align-self-center">
-            </div>
-            <div class="col-6">
-                <div class="row mb-3 d-flex align-items-center">
-                    <div class="col-4 py-2">
-                        <img src="/assets/weather-png/temperature.png" alt="current temp" class="img-fluid">
-                    </div>
-                    <div class="col-8">
-                    <span class="">${Math.round(data.current.temp)}&#8457</span>
-                    </div>
-                </div>
-                <div class="row bg-light mb-3 d-flex align-items-center">
-                    <div class="col-4 py-2">
-                        <img src="/assets/weather-png/umbrella.png" alt="current temp" class="img-fluid">
-                    </div>
-                    <div class="col-8">
-                    <span class="">${data.hourly[0].pop * 100}%</span>
-                    </div>
-                </div>
-                <div class="row mb-3 d-flex align-items-center">
-                    <div class="col-4 py-2">
-                        <img src="/assets/weather-png/wind.png" alt="current temp" class="img-fluid">
-                    </div>
-                    <div class="col-8">
-                    <span class="">${data.current.wind_speed} mph</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer row">
-            <h3 class="text-center">${data.current.weather[0].description}</h3>
-        </div>
-      </div>
-    `
-    );
-  }
-
-  function getDate(date) {
-    return new Date(date * 1000).toLocaleString("en-US", {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
-    });
-  }
-
-  function setLocationName(data) {
-    let locationName = "";
-    $.get("http://api.openweathermap.org/geo/1.0/reverse", {
-      APPID: weatherKey,
-      lat: data.lat,
-      lon: data.lon,
-    }).done(function (locationData) {
-      locationName = locationData[0].name;
-      setCurrentWeatherDiv(data, locationName);
-      setForecastedWeather(data);
-    });
-  }
-
-  function setForecastedWeather(data) {
-    forecastedWeatherTilesDiv.innerHTML = getForecastedWeatherTiles(data);
-  }
-
-  /*
-  ///////////////////////////////////////////////////////////////////////
-  Create Weather Tiles HTML
-  ///////////////////////////////////////////////////////////////////////
-   */
-  function getForecastedWeatherTiles(data) {
-    let forecastedDayCount = 5;
-    let html = "";
-    for (let i = 1; i < forecastedDayCount; i++) {
-      let weatherGif = getWeatherGif(data.daily[i].weather[0].id);
-
-      html += '<div class="card col-5 col-md p-0 mx-auto mx-md-2 my-2">';
-      html += '<div class="card-header colorMe text-white p-3 p-md-2 pt-3">';
-      html += "<h3 class='text-center'>" + getDate(data.daily[i].dt) + "</h3>";
-      html += "</div>";
-
-
-      html += '<div class="card-body">';
-      html +=
-          "<img src=" +
-          weatherGif +
-          ' alt="weather gif" class="img-fluid align-self-center">';
-      html += "</div>";
-
-
-      html +=
-          '<div class="card-footer bg-light d-flex justify-content-between p-0 pt-3">';
-      html += '<div class="col-4 p-0 text-center">';
-      html +=
-          '<img src="/assets/weather-png/cold.png" alt="low temp" class="img-fluid w-50 mb-2">';
-      html +=
-          "<p>" +
-          Math.round(data.daily[i].temp.min) +
-          "<span class='unit'>&#8457</span></p>";
-      html += "</div>";
-
-
-      html += '<div class="col-4 p-0 text-center">';
-      html +=
-          '<img src="/assets/weather-png/hot.png" alt="high temp" class="img-fluid w-50 mb-2">';
-      html +=
-          "<p>" +
-          Math.round(data.daily[i].temp.max) +
-          "<span class='unit'>&#8457</span></p>";
-      html += "</div>";
-
-
-      html += '<div class="col-4 p-0 text-center">';
-      html +=
-          '<img src="/assets/weather-png/umbrella.png" alt="chance of rain" class="img-fluid w-50 mb-2">';
-      html +=
-          "<p>" +
-          Math.round(data.daily[i].pop * 100) +
-          "<span class='unit'>%</span></p>";
-      html += "</div>";
-
-
-      html += "</div>";
-      html += "</div>";
-    }
-    return html;
-  }
-
-  /*
-  ///////////////////////////////////////////////////////////////////////
-  Determines which gif to pull from assets from api
-  ///////////////////////////////////////////////////////////////////////
-   */
-  function getWeatherGif(weatherState) {
-    let weatherGifSource = "";
-    if (weatherState < 200) {
-      weatherGifSource = "/assets/weather-gifs/rainbow.gif";
-    }
-    if (weatherState < 300 && weatherState >= 200) {
-      weatherGifSource = "/assets/weather-gifs/storm.gif";
-    }
-    if (weatherState < 400 && weatherState >= 300) {
-      weatherGifSource = "/assets/weather-gifs/drizzle.gif";
-    }
-    if (weatherState < 500 && weatherState >= 400) {
-      weatherGifSource = "/assets/weather-gifs/rainbow.gif";
-    }
-    if (weatherState < 600 && weatherState >= 500) {
-      if (weatherState === 511) {
-        weatherGifSource = "/assets/weather-gifs/icicles.gif";
-      }
-      weatherGifSource = "/assets/weather-gifs/rain.gif";
-    }
-    if (weatherState < 700 && weatherState >= 600) {
-      weatherGifSource = "/assets/weather-gifs/snow-2.gif";
-    }
-    if (weatherState < 800 && weatherState >= 700) {
-      if (weatherState === 781) {
-        weatherGifSource = "/assets/weather-gifs/whirlwind.gif";
-      }
-      weatherGifSource = "/assets/weather-gifs/foggy.gif";
-    }
-    if (weatherState === 800) {
-      weatherGifSource = "/assets/weather-gifs/sun.gif";
-    }
-    if (weatherState === 801) {
-      weatherGifSource = "/assets/weather-gifs/cloudy.gif";
-    }
-    if (weatherState >= 802) {
-      weatherGifSource = "/assets/weather-gifs/clouds.gif";
-    }
-    return weatherGifSource;
   }
 })();
